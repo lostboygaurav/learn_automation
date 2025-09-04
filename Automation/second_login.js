35c3e6bb182b7a7559671873f73aa9d926462053
lostboygaurav <gauravgupta@91rucks.com>
2025-09-04 10:04:32 +0530

    fourth commit

---
+++ Automation/second_login.js
@@ -0,0 +1,44 @@
+// install playwright first: npm install @playwright/test
+
+const { chromium } = require('@playwright/test');
+
+(async () => {
+  const browser = await chromium.launch({ headless: false }); // headless:false => show browser
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  // Go to your website
+  await page.goto('https://devcv.91trucks.com/');
+
+  // Click on "Login with Google"
+  await page.click('text=Sign in with Google');  // adjust selector based on your button
+
+  // Wait for Google login popup
+  const [popup] = await Promise.all([
+    // context.waitForEvent('page', { timeout: 30000}),
+    page.waitForSelector('input[type="email"]'),
+    // page.click('text=Sign in with Google') // triggers popup
+  ]);
+
+  // Fill Google login form
+  // await page.waitForTimeout(30000)
+  await popup.fill('gaurav.gupta@91trucks.com');
+  await page.click('text=Next')
+
+  passwordField = await page.waitForSelector('input[type="password"]');
+  await passwordField.fill('Trucks@123');
+  await page.click('text=Next');
+
+  // Wait until redirected back to your app
+  await page.waitForURL('https://devcv.91trucks.com/dashboard');  // replace with your app’s post-login page
+  await page.waitForTimeout(10000),
+  await page.waitForURL('https://devcv.91trucks.com/deal');  // replace with your app’s post-login page
+  await page.waitForTimeout(10000),
+  await page.waitForURL('https://devcv.91trucks.com/customers');  // replace with your app’s post-login page
+  await page.waitForTimeout(10000),
+
+  console.log("✅ Successfully logged in with Google!");
+
+  // Close
+  await browser.close();
+})();
